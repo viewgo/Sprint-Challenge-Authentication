39068b8923cd92bd37e96ce078e1e187ddd83253
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-12-20 13:06:12 -0600

    Testing finally works

--- api/server.spec.js
+++ api/server.spec.js
@@ -4,7 +4,7 @@ const db = require("../database/dbConfig.js");
 const Users = require("../users/users-model.js");
 
 describe("auth-router.js", function() {
-  beforeEach(async () => {
+  beforeAll(async () => {
     await db("users").truncate();
   });
 
@@ -19,7 +19,12 @@ describe("auth-router.js", function() {
         });
     });
 
-    it.skip("register jaytee", async function() {
+    it("find hugo", async function() {
+      const users = await db("users");
+      console.log(users);
+    });
+
+    it("register jaytee", async function() {
       return request(app)
         .post("/api/auth/register")
         .send({ username: "jaytee", password: "jaytee" })
@@ -29,10 +34,29 @@ describe("auth-router.js", function() {
         });
     });
 
-    it("login and get", async function() {
+    it("login and get hugo", async function() {
       return request(app)
         .post("/api/auth/login")
         .send({ username: "hugo", password: "hugo" })
+        .then(res => {
+          console.log("RES.BODY!!!!!!!!!!!!!!!!!!", res.body);
+          const token = res.body.token;
+
+          return request(app)
+            .get("/api/jokes")
+            .set("authorization", token)
+            .then(res => {
+              console.log(res.status);
+              expect(res.status).toBe(200);
+              expect(Array.isArray(res.body)).toBe(true);
+            });
+        });
+    });
+
+    it("login and get hugo", async function() {
+      return request(app)
+        .post("/api/auth/login")
+        .send({ username: "jaytee", password: "jaytee" })
         .then(res => {
           console.log("RES.BODY!!!!!!!!!!!!!!!!!!", res.body);
           const token = res.body.token;
