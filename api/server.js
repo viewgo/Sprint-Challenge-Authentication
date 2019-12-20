16c401c204cfa1bfd09eb97c683e39048468c52e
Hugo Oliveira <oliveira.hugo.c@gmail.com>
2019-12-20 13:40:00 -0600

    basic react app to show jokes for stretch

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ server.use(cors());
 server.use(express.json());
 
 server.use("/api/auth", authRouter);
-server.use("/api/jokes", jokesRouter);
+server.use("/api/jokes", authenticate, jokesRouter);
 
 server.get("/test", async (req, res) => {
   res.json({ message: "pass!" });
